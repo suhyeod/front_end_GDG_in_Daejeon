b1f3e9202fff39eaf53db6345fed7835007a0ea6
최수혁 <choisuhyeok@choesuhyeog-ui-noteubug.local>
2024-09-11 23:23:46 +0900

    tlqkfsusdk

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import GDSC_LOGO from "./image/GDSC.png";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="body">
+            <header>
+                <div class="title">
+                    <a href='/'>
+                        <img src={GDSC_LOGO}/>
+                    </a>
+                </div>
+                <ul class="navbar" id="navbar">
+                    <li>
+                        <a href="#home">Home</a>
+                    </li>
+                    <li>
+                        <a href="#about">About</a>
+                    </li>
+                    <li>
+                        <a href="#service">Services</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+                </ul>                
+            </header>
+            <main>
+              <section className="section_1">
+              <ul class="home" id="home">
+                  <h1>
+
+                  </h1>
+                  </ul>
+                </section>
+                <section className="section_2">
+                <ul class="about" id="about">
+                    <h1>This is About Page of Coding Crusaders</h1>
+                    <p></p>
+                </ul>
+                
+                </section>
+                <section className="section_3">
+                <ul class="service" id="service">
+                    <h1>This is Service Page of Coding Crusaders</h1>
+                </ul>
+               
+                </section>
+                <section className="section_4">
+                <ul class="contact" id="contact">
+                    <h1>This is contact Page of Coding Crusaders</h1>
+                </ul>
+                
+                </section>
+            </main>
+            <footer></footer>
+        </div>
+    );
 }
 
 export default App;
